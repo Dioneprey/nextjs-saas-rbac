3e0f99a87ce782038a099750fbf57c1681796f18
Dionatan <dionatan.oliveira@bttis.com>
2024-05-29 07:41:24 -0300

    create account route

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user_id' })
-
-const project = projectSchema.parse({ id: 'project_id', ownerId: 'user_id' })
-
-console.log(ability.can('delete', project))
